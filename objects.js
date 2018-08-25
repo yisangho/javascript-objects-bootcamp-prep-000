3be4f254e1a47049c792ce1c2c758965d92f2ef5
yisangho <yisangho717@gmail.com>
2018-08-25 18:08:47 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -5,7 +5,7 @@ function updatePlaylist(obj, key, value) {
   console.log(obj);
   console.log(key);
   console.log(value);
-  return Object.assign({}, obj, { [key]: [value] })
+  return Object.assign({}, obj, { [key]: value })
 }
 
 function removeFromPlaylist(obj, key) {
