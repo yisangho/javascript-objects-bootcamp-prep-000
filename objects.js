7c2cd50e5e7ed6f2bbb8788d16e7af0b63efb444
yisangho <yisangho717@gmail.com>
2018-08-25 18:02:29 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -2,7 +2,7 @@ var playlist = { bbb : "song1",
                  ccc : "song2"}
 
 function updatePlaylist(obj, key, value) {
-  return Object.assign({}, obj, { key: [value] })
+  return Object.assign({}, obj, { [key]: [value] })
 }
 
 function removeFromPlaylist(obj, key) {
